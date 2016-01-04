09f50ca80be2f3a1095157cb717ec3113ce90b5d
Dipen Sekhsaria <dipen0510@gmail.com>
2016-01-05 00:31:55 +0530

    Initial Commit

---
+++ CoachingClass/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoachingClass
+//
+//  Created by Dipen Sekhsaria on 05/01/16.
+//  Copyright © 2016 Star Deep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoachingClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoachingClass
+//
+//  Created by Dipen Sekhsaria on 05/01/16.
+//  Copyright © 2016 Star Deep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
